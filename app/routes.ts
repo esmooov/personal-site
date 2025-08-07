8effe83ef9793ca4e7bdc645f81e34a2e43b111a
Erik Hinton <erikhinton@Eriks-MacBook-Pro-2.local>
2025-08-06 22:26:27 -0400

    basic site

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  route("/:subpage?", "routes/home.tsx", [
+    index("routes/empty.tsx"),
+    route(":file", "routes/file.tsx"),
+  ]),
+] satisfies RouteConfig;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,127 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Outlet, useParams } from "react-router";
+import styled from "styled-components";
+
+const MainPage = styled.div`
+  font-family: "lores-15", sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  padding: 40px;
+
+  h1 {
+    font-size: 62px;
+  }
+  
+`
+
+const InnerPage = styled.div`
+  border: 3px double black;
+  min-height: calc(100vh - 80px);
+  padding: 20px 40px;
+  display: flex;
+`
+
+const LinkBar = styled.div`
+  display: flex;
+  gap: 8px;
+  font-family: lores-21-serif, sans-serif;
+  font-weight: 400;
+  padding-bottom: 8px;
+  font-size: 28px;
+
+  a {
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+
+    &:hover {
+      background-color: black;
+      color: white;
+    }
+  }
+`
+
+const Items = styled.div`
+  border: 4px groove gray;
+  padding: 10px;
+`
+
+const Left = styled.div`
+  width: 50%;
+`
+
+const Right = styled.div`
+  width: 50%;
+  padding: 20px
+`
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Erik Hinton's WebHome" },
+    { name: "description", content: "Welcome to my internet!" },
   ];
 }
 
+type ItemEntry = {
+  name: string,
+  path: string
+}
+
+const Files: Record<string, Array<ItemEntry>> = {
+  "": [],
+  "writing": [
+    {name: "Foo Bar", path: "foo"}
+  ]
+}
+
+const Item = ({name, path, subpage}: ItemEntry & {subpage?: string}) => {
+  return (
+    <a href={`/${subpage}/${path}`}>
+      {name}
+    </a>
+  )
+}
+
 export default function Home() {
-  return <Welcome />;
+  const {subpage} = useParams()
+  const files = Files[subpage || ""] || [];
+  return <MainPage>
+    <InnerPage>
+      <Left>
+        <h1>
+          <div>
+            erik hinton
+          </div>
+        </h1>
+        <LinkBar>
+          <a href="/writing">
+            <div>
+              <p>Writing</p>
+            </div> 
+          </a>
+          <a href="/print">
+            <div>
+              <p>Print</p>
+            </div> 
+          </a>
+          <a href="#">
+            <div>
+              <p>Events</p>
+            </div> 
+          </a>
+          <a href="#">
+            <div>
+              <p>Misc.</p>
+            </div> 
+          </a>
+        </LinkBar>
+        <Items>
+          {files.map(f => <Item key={f.name} name={f.name} path={f.path} subpage={subpage} />)}
+        </Items>
+      </Left>
+      <Right>
+        <Outlet/>
+      </Right>
+    </InnerPage>
+  </MainPage>
 }
